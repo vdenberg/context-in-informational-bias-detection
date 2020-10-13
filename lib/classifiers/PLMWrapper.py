c9029f740c11e48b9f5222d042aa1d6fd898772a
vdberg <vdberg@cl.uni-heidelberg.de>
2020-10-13 13:50:23 +0200

    debugging finetuning

--- lib/classifiers/PLMWrapper.py
+++ lib/classifiers/PLMWrapper.py
@@ -494,6 +494,7 @@ class Inferencer():
     def evaluate(self, model=None, data=None, labels=None, preds=None, av_loss=None, set_type='dev', name='Basil', output_mode='sent_clf'):
         if preds is None:
             preds, _ = self.predict(model, data, output_mode=output_mode)
+            labels = labels.flatten()
         else:
             if output_mode == 'tok_clf':
                 preds = arrays_in_series(preds)
