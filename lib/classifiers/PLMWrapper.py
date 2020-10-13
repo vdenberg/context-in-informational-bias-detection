175d5f4f27bf6870402e8cb675463421937d1c8d
vdberg <vdberg@cl.uni-heidelberg.de>
2020-10-13 22:40:55 +0200

    debugging finetuning

--- lib/classifiers/PLMWrapper.py
+++ lib/classifiers/PLMWrapper.py
@@ -630,14 +630,10 @@ class Inferencer():
                 pred = np.argmax(logits, axis=1)[0]
 
             elif output_mode == 'seq_sent_clf':
-                print('hm')
-                print(logits)
                 pred = logits[0].argmax(axis=1).tolist()
-                print(pred)
                 pad = [-1] * (len(label_ids) - len(pred))
                 pred += pad
                 pred = np.asarray(pred)
-                print(pred)
 
             preds.append(pred)
 
