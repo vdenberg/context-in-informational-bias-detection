dcc058b85a8c20fd609910e32ea56909f19490b0
vdberg <vdberg@cl.uni-heidelberg.de>
2020-10-13 11:30:50 +0200

    debugging finetuning

--- lib/classifiers/PLMWrapper.py
+++ lib/classifiers/PLMWrapper.py
@@ -494,7 +494,7 @@ class Inferencer():
         if preds is None:
             preds, _ = self.predict(model, data, output_mode=output_mode)
         else:
-            preds = [re.sub("\[\]'", '', el) for el in preds]
+            preds = [el.strip("'").strip('[]') for el in preds]
             preds = [tuple(map(int, el.split(', '))) for el in preds]
             print(preds)
 
