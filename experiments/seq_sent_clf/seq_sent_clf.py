5a6d855429a1b4cef655f8db29038537d21613f3
vdberg <vdberg@cl.uni-heidelberg.de>
2020-10-13 16:36:05 +0200

    debugging finetuning

--- experiments/seq_sent_clf/seq_sent_clf.py
+++ experiments/seq_sent_clf/seq_sent_clf.py
@@ -75,7 +75,7 @@ args = parser.parse_args()
 
 N_EPS = args.n_epochs
 SAMPLER = args.sampler
-CLF_TASK = args.clf_task
+CLF_TASK = 'seq_sent_len'
 SPLIT = args.split
 MODEL = args.model
 WINDOW = args.window
