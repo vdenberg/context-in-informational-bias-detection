c1bd758fbb4a2d885378f7baad1e961f09b97544
vdberg <vdberg@cl.uni-heidelberg.de>
2020-10-14 12:26:43 +0200

    debugging finetuning

--- lib/classifiers/PLMWrapper.py
+++ lib/classifiers/PLMWrapper.py
@@ -662,9 +662,14 @@ class Inferencer():
                 labels = np.asarray(labels).flatten()
                 preds = np.asarray(preds).flatten()
         else:
+            print(type(labels), type(labels[0]), labels[0])
+            print(type(preds), type(preds[0]), preds[0])
             if output_mode != 'sent_clf':
                 preds = arrays_in_series(preds)
                 labels = arrays_in_series(labels)
+            print(type(labels), type(labels[0]))
+            print(type(preds), type(preds[0]))
+            exit(0)
 
         if output_mode == 'seq_sent_clf':
             m = labels != -1
