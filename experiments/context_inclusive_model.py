9203095fac3305b86cef430e4f9f99d058622704
vdberg <vdberg@cl.uni-heidelberg.de>
2020-10-14 18:42:48 +0200

    debugging cim

--- experiments/context_inclusive_model.py
+++ experiments/context_inclusive_model.py
@@ -309,7 +309,7 @@ logger.info(f" Max doc len: {MAX_DOC_LEN}")
 data = pd.read_json(DATA_FP)
 data.index = data.sentence_ids.values
 
-spl = Split(data, subset=SUBSET, recreate=PREPROCESS)
+spl = Split(data, subset=SUBSET, recreate=False, sv=99)
 folds = spl.apply_split(features=['story', 'source', 'id_num', 'art_context_doc_num', 'cov1_context_doc_num', 'cov2_context_doc_num', 'token_ids', 'token_mask', 'position', 'quartile', 'src_num'])
 if DEBUG:
     folds = [folds[0]] #, folds[1]
