a8e837326186bb646c70678b58f8cb4e213fd7e6
vdberg <vdberg@cl.uni-heidelberg.de>
2020-10-15 17:18:41 +0200

    predictions and analysis

--- lib/ErrorAnalysis.py
+++ lib/ErrorAnalysis.py
@@ -133,7 +133,7 @@ class ErrorAnalysis:
             mets, _ = my_eval(gr.bias, gr[f'{model}{i}'])
             cross_mets.append(np.asarray([mets['prec'], mets['rec'], mets['f1']]))
         cross_mets = np.asarray(cross_mets)
-        mets = np.mean(cross_mets, axis=2)
+        mets = np.mean(cross_mets, axis=0)
         mets = [round(el*100, 2) for el in mets]
 
         return [n] + lat([N, Percbias] + mets)
