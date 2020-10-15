53b47b1cc2fe3c2fefd81c7b5e2408685e6e0e1c
vdberg <vdberg@cl.uni-heidelberg.de>
2020-10-15 18:05:24 +0200

    predictions and analysis

--- lib/ErrorAnalysis.py
+++ lib/ErrorAnalysis.py
@@ -114,6 +114,7 @@ class ErrorAnalysis:
             for i, f in enumerate(os.listdir(pred_dir)):
                 n = f'{model}{i}'
                 subdf = pd.read_csv(os.path.join(pred_dir, f), index_col=0)
+                print(subdf.columns)
                 print(subdf.head())
                 out[n] = subdf.pred
         return out
