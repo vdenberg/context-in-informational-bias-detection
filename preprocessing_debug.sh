61415d66655c8797ed01fe6cbf54ca4fce23d974
vdberg <vdberg@cl.uni-heidelberg.de>
2020-10-16 10:16:00 +0200

    readme

--- preprocessing_debug.sh
+++
@@ -1,4 +0,0 @@
-touch data/inputs/tapt/basil_train.tsv
-touch data/inputs/tapt/basil_test.tsv
-python3 preprocessing/preprocessing_for_cim_and_tapt.py # --add_use --add_sbert
-python3 preprocessing/preprocessing_for_plm.py --clf_task seq_sent_clf -plm roberta -seqlen 10 -win
