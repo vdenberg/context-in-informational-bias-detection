681b757be196b5b464090faa6478f932597fd036
vdberg <vdberg@cl.uni-heidelberg.de>
2020-10-22 15:23:41 +0200

    eval

--- analyses/in_depth_analysis.py
+++ analyses/in_depth_analysis.py
@@ -27,7 +27,7 @@ print('Difference in performance depending on sentence length:')
 print(sentlen_df.to_latex())
 
 # IN QUOTE OR NOT + PRESENCE OF QUOTATION MARKS
-quote_comparison = [ea.compare_subsets(ea.w_preds, 'inf_quote', model, metrics=['red'], inf_bias_only=True) for model, _ in ea.models]
+quote_comparison = [ea.compare_subsets(ea.w_preds, 'inf_quote', model, metrics=['rec'], inf_bias_only=True) for model, _ in ea.models]
 quote_df = ea.concat_comparisons(quote_comparison)
 
 print('Difference in performance depending on annotated as in a quote:')
