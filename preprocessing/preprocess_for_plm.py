7c327b7e6c167ff6ecb1b98c2cc2c3c16b861b1e
vdberg <vdberg@cl.uni-heidelberg.de>
2021-01-11 14:33:26 +0100

    update with source adaptation

--- preprocessing/preprocess_for_plm.py
+++ preprocessing/preprocess_for_plm.py
@@ -235,8 +235,7 @@ if __name__ == '__main__':
 
     basil = pd.read_csv('data/basil.csv', index_col=0).fillna('')
     if SOURCE != 'all':
-        print(basil.source.value_counts())
-        exit(0)
+        basil.source = basil.source.asstr.lower()
         basil = basil[basil.source == SOURCE]
 
     if not os.path.exists(DATA_TSV_IFP):
