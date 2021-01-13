9d3b123290e99be259ed5bf43f6412f779f4d9c9
vdberg <vdberg@cl.uni-heidelberg.de>
2021-01-13 15:57:50 +0100

    update with source adaptation

--- preprocessing/preprocess_for_cim_and_tapt.py
+++ preprocessing/preprocess_for_cim_and_tapt.py
@@ -190,13 +190,12 @@ def preprocess_cc_for_tapt(train_ifp="data/inputs/tapt/cc/fox", train_ofp="data/
         ifp = os.path.join(train_ifp, fn)
         content = json.load(open(ifp))
         text = content['maintext']
-        text = re.sub("\n", ' ', text)
-        #sentences = [s.text for s in nlp(text).sents]
+        sentences = [s.text for s in nlp(text).sents]
 
         with open(train_ofp, 'a') as f:
-            #for s in sentences:
-            #    f.write(s)
-            #    f.write(' ')
+            for s in sentences:
+                f.write(s)
+                f.write(' ')
             f.write(text)
             f.write('\n')
 
