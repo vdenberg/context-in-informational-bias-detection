a39120ca1fcf25149bc69d11d861e8146f05b6fa
vdberg <vdberg@cl.uni-heidelberg.de>
2021-01-21 14:52:43 +0100

    update for source adaptation

--- preprocessing/preprocess_for_cim_and_tapt.py
+++ preprocessing/preprocess_for_cim_and_tapt.py
@@ -231,7 +231,7 @@ def preprocess_basil_for_dsp(data, data_dir, recreate=False, source=None):
         stats_dir = os.path.dirname(data_dir)
         size = sum(fold['sizes'])
         tmp = {"data_dir": stats_dir, "dataset_size": size}
-        datasets_string = f'{name}]: {tmp}'
+        datasets_string = f'{name}: {tmp}'
         print(datasets_string)
     return folds
 
