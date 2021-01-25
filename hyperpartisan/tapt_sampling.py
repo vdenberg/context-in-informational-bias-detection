2216326632892b260223eca7bda5713bf566130c
vdberg <vdberg@cl.uni-heidelberg.de>
2021-01-25 15:31:47 +0100

    hp reproduction

--- hyperpartisan/tapt_sampling.py
+++ hyperpartisan/tapt_sampling.py
@@ -1,8 +1,9 @@
 import xml.etree.ElementTree as ET
 import pandas as pd
+from bs4 import BeautifulSoup
+
 in_fp = '../experiments/dont-stop-pretraining/data/hyperpartisan/unlabeled/articles-training-bypublisher-20181122.xml'
 out_fp = '../experiments/dont-stop-pretraining/data/hyperpartisan/unlabeled/input.txt'
-import random
 
 with open(in_fp, 'r') as f:
     content = f.readlines()
@@ -23,7 +24,8 @@ for tna in tags_n_articles[:5000]:
     for l in [firstline] + lines[1:]:
         text_lines.append(l[3:-5])
     text = ' '.join(text_lines)
-    texts.append(text)
+    cleantext = BeautifulSoup(text, "lxml").text
+    texts.append(cleantext)
 
 with open(out_fp, 'w') as f:
     for t in texts:
