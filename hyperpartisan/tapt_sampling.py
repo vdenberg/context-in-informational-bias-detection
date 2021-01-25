8cf881a9252c9ccfb023d9ac56351e5f26642bf8
vdberg <vdberg@cl.uni-heidelberg.de>
2021-01-25 12:07:49 +0100

    update for source adaptation

--- hyperpartisan/tapt_sampling.py
+++ hyperpartisan/tapt_sampling.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup as bs
 import lxml
 
-with open("experiments/dont-stop-pretraining/data/hyperpartisan/unlabeled/articles-training-bypublisher-20181122.xml", "r") as f:
+with open("../experiments/dont-stop-pretraining/data/hyperpartisan/unlabeled/articles-training-bypublisher-20181122.xml", "r") as f:
     content = f.readlines()
     content = "".join(content)
     bs_content = bs(content, "lxml")
