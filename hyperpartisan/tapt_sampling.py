cbfddec1ceb45f2df847751ba18bf12971ae1ac1
vdberg <vdberg@cl.uni-heidelberg.de>
2021-01-25 12:33:05 +0100

    hp reproduction

--- hyperpartisan/tapt_sampling.py
+++ hyperpartisan/tapt_sampling.py
@@ -2,9 +2,14 @@ import xml.etree.ElementTree as ET
 
 in_fp = '../experiments/dont-stop-pretraining/data/hyperpartisan/unlabeled/articles-training-bypublisher-20181122.xml'
 
-tree = ET.parse(in_fp)
-root = tree.getroot()
+with open(in_fp, 'r') as f:
+    content = f.readlines()
+    content = ''.join(content[2:-1])
+    tags_n_articles = content.split('</article>')
 
-for member in root.findall('article'):
-    print(member)
-    print(member.find('article'))
+for tna in tags_n_articles[:10]:
+    print(tna)
+
+#for tna in tags_n_articles:
+#    a = ''.join(tna.split('>')[2:])
+
