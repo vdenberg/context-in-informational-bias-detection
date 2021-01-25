1bc1e78afe78a3124fa9d40b8c3b3ec56ee1f8f6
vdberg <vdberg@cl.uni-heidelberg.de>
2021-01-25 12:51:50 +0100

    hp reproduction

--- hyperpartisan/tapt_sampling.py
+++ hyperpartisan/tapt_sampling.py
@@ -1,23 +1,33 @@
 import xml.etree.ElementTree as ET
-
+import pandas as pd
 in_fp = '../experiments/dont-stop-pretraining/data/hyperpartisan/unlabeled/articles-training-bypublisher-20181122.xml'
+out_fp = '../experiments/dont-stop-pretraining/data/hyperpartisan/unlabeled/input.txt'
+import random
 
 with open(in_fp, 'r') as f:
     content = f.readlines()
     content = ''.join(content[2:-1])
     tags_n_articles = content.split('</article>')
 
-for tna in tags_n_articles[:2]:
+random.shuffle(tags_n_articles)
+
+texts = []
+for tna in tags_n_articles[:5000]:
     lines = tna.strip('\n').split('\n')
-    print(lines[0])
     tag, firstline = lines[0].split('<p>')
     firstline = '<p>' + firstline
     text_lines = []
     for l in [firstline] + lines[1:]:
         text_lines.append(l[3:-5])
-    print(tag)
-    print(text_lines[:3])
-    print('---')
+    text = ' '.join(text_lines)
+    texts.append(text)
+
+with open(out_fp, 'w') as f:
+    for t in texts:
+        f.write(t)
+        f.write('\n')
+
+
 
 
 #for tna in tags_n_articles:
