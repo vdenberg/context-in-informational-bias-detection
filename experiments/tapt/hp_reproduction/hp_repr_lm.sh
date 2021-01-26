b7456b5f602ba648e0325bdf83d8043a25e873ac
vdberg <vdberg@cl.uni-heidelberg.de>
2021-01-26 14:50:47 +0100

    hp reproduction

--- experiments/tapt/hp_reproduction/hp_repr_lm.sh
+++ experiments/tapt/hp_reproduction/hp_repr_lm.sh
@@ -1,8 +1,14 @@
+wget https://zenodo.org/record/1489920/files/articles-training-bypublisher-20181122.zip?download=1
+mv articles-training-bypublisher-20181122.zip?download=1 articles-training-bypublisher-20181122.zip
+unzip articles-training-bypublisher-20181122.zip && rm articles-training-bypublisher-20181122.zip
+/opt/slurm/bin/srun --partition compute --mem 20GB python hp_preprocess.py
+
+cd ../dont-stop-pretraining
 rm pretrained_models/re_hp_515 && mkdir pretrained_models/re_hp_515
 /opt/slurm/bin/srun --partition kama --gres=gpu:1 --mem 20GB python -m scripts.run_language_modeling \
                                         --train_data_file ../data/hyperpartisan/train.txt \
                                         --line_by_line \
-                                        --output_dir hp_reproduction/re_hp_515 \
+                                        --output_dir pretrained_models/re_hp_515 \
                                         --model_type roberta-base \
                                         --tokenizer_name roberta-base \
                                         --mlm \
@@ -16,13 +22,13 @@ rm pretrained_models/re_hp_515 && mkdir pretrained_models/re_hp_515
                                         --num_train_epochs 100  \
                                         --learning_rate 0.0001 \
                                         --logging_steps 50 \
-                                        --seed $SEED
+                                        --seed 11
 
-rm pretrained_models/hp_5015 && mkdir pretrained_models/hp_5015
+rm pretrained_models/re_hp_5000 && mkdir pretrained_models/re_hp_5000
 /opt/slurm/bin/srun --partition kama --gres=gpu:1 --mem 20GB python -m scripts.run_language_modeling \
                                         --train_data_file ../data/hyperpartisan/curated.txt \
                                         --line_by_line \
-                                        --output_dir hp_reproduction/re_hp_5015\
+                                        --output_dir pretrained_models/re_hp_5000\
                                         --model_type roberta-base \
                                         --tokenizer_name roberta-base \
                                         --mlm \
@@ -36,4 +42,14 @@ rm pretrained_models/hp_5015 && mkdir pretrained_models/hp_5015
                                         --num_train_epochs 100  \
                                         --learning_rate 0.0001 \
                                         --logging_steps 50 \
-                                        --seed $SEED
+                                        --seed 11
+
+mkdir model_logs/re_hp_515_ft/
+/opt/slurm/bin/srun --partition kama --gres=gpu:1  --mem 2GB python -m scripts.train --device 0 --perf +f1 --evaluate_on_test \
+                --hyperparameters ROBERTA_CLASSIFIER_MINI \
+                --config training_config/classifier.jsonnet \
+                --serialization_dir model_logs/hyperpartisan_tapt/ \
+                --dataset hyperpartisan_news \
+                --model roberta-base \
+                --jackknife \
+                -x 11 22 33 44 55
