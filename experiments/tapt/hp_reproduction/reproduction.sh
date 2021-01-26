76f2999dcd526fb0a64fa715c8117120e17eea8d
vdberg <vdberg@cl.uni-heidelberg.de>
2021-01-26 10:23:55 +0100

    hp reproduction

---
+++ experiments/tapt/hp_reproduction/reproduction.sh
@@ -0,0 +1,18 @@
+/opt/slurm/bin/srun --partition kama --gres=gpu:1  --mem 20GB python -m scripts.run_language_modeling \
+                                        --train_data_file data/hp_reproduction/unlabeled/input.txt \
+                                        --line_by_line \
+                                        --output_dir hp_reproduction-tapt \
+                                        --model_type roberta-base \
+                                        --tokenizer_name roberta-base \
+                                        --mlm \
+                                        --per_gpu_train_batch_size 16 \
+                                        --gradient_accumulation_steps 16  \
+                                        --model_name_or_path roberta-base \
+                                        --eval_data_file data/hp_reproduction/dev.jsonl \
+                                        --do_eval \
+                                        --evaluate_during_training  \
+                                        --do_train \
+                                        --num_train_epochs 100  \
+                                        --learning_rate 0.0001 \
+                                        --logging_steps 50 \
+                                        --seed 11
