427c4f3e673fa3e86d91cea7fada7ff4bb2f5062
vdberg <vdberg@cl.uni-heidelberg.de>
2021-01-29 11:29:36 +0100

    preprocess for domain adaptation

--- preprocessing/preprocess_for_cim.py
+++ preprocessing/preprocess_for_cim.py
@@ -3,7 +3,7 @@ import argparse, os
 from lib.handle_data.BasilLoader import LoadBasil
 from lib.handle_data.SplitData import Split
 import json, re, random
-
+import spacy
 
 def tokenize(x):
     global nlp
@@ -152,120 +152,6 @@ def preprocess_for_cim(basil, add_use=False, add_sbert=False, ofp="data/inputs/c
                         f.write(line)
                         f.write('\n')
 
-
-def write_for_dsp_lm(df, fp):
-    sentences = list(df.sentence.values)
-    with open(fp, 'w') as f:
-        for s in sentences:
-                f.write(s)
-                f.write('\n')
-
-
-def preprocess_basil_for_dsp_lm(basil, eval_size=20, data_dir="data/tapt/basil_and_source_tapt/data", source=None):
-    """
-    Preprocess for language modeling
-    """
-    if source:
-        train_ofp = os.path.join(data_dir, f'{source}_basil_train.txt')
-        eval_ofp = os.path.join(data_dir, f'{source}_basil_eval.txt')
-        eval_size = int(eval_size/3)
-        basil = [basil['source'] == source]
-    else:
-        train_ofp = os.path.join(data_dir, 'basil_train.txt')
-        eval_ofp = os.path.join(data_dir, 'basil_eval.txt')
-
-    articles = basil.article.unique()
-    random.shuffle(articles)
-    train_articles = articles[:eval_size]
-    test_articles = articles[eval_size:]
-
-    train_df = basil[basil.article in train_articles]
-    eval_df = basil[basil.article in test_articles]
-
-    write_for_dsp_lm(train_df, train_ofp)
-    write_for_dsp_lm(eval_df, eval_ofp)
-    print(f'Wrote {len(train_df)} to {train_ofp}')
-    print(f'Wrote {len(eval_df)} to {train_ofp}')
-
-
-def write_for_dsp_train(data, fp):
-    with open(fp, 'w') as f:
-        ids = data.id.values
-        sentences = data.sentence.values
-        labels = data.label.values
-        for i, s, l in zip(ids, sentences, labels):
-            instance = {'text': s, 'label': str(l), 'metadata': [i]}
-            json.dump(instance, f)
-            f.write('\n')
-
-
-def preprocess_basil_for_dsp_train(data, data_dir, recreate=False, source=None):
-    ''' This function selects those columns which are relevant for creating input for finetuning with DSP
-    code our data, and saves them for each fold seperately. '''
-    data['id'] = data.index
-
-    # split data into folds
-    spl = Split(data, which='both', recreate=False, sv=99)
-    folds = spl.apply_split(features=['id', 'label', 'sentence'], source=source)
-
-    data_str_fp = os.path.join(data_dir, 'stats.json')
-    data_strs = {}
-
-    # write data for each fold
-    for i, fold in enumerate(folds):
-        fold_dir = os.path.join(data_dir, str(fold['name']))
-        if not os.path.exists(fold_dir):
-            os.mkdir(fold_dir)
-
-        train_ofp = os.path.join(fold_dir, f"train.jsonl")
-        dev_ofp = os.path.join(fold_dir, f"dev.jsonl")
-        test_ofp = os.path.join(fold_dir, f"test.jsonl")
-
-        if not os.path.exists(train_ofp) or recreate:
-            write_for_dsp_train(fold['train'], train_ofp)
-
-        if not os.path.exists(dev_ofp) or recreate:
-            write_for_dsp_train(fold['dev'], dev_ofp)
-
-        if not os.path.exists(test_ofp) or recreate:
-            write_for_dsp_train(fold['test'], test_ofp)
-
-        if source:
-            name = f"{source}_basil_{fold['name']}"
-        else:
-            name = f"basil_{fold['name']}"
-
-        stats_dir = '/'.join(fold_dir.split('/')[-2:])
-        size = sum(fold['sizes'])
-        tmp = {"data_dir": stats_dir + "/", "dataset_size": size}
-        data_strs[name] = tmp
-
-    with open(data_str_fp, 'a') as f:
-        json.dump(data_strs, f)
-
-    return folds
-
-
-def preprocess_cc_for_dsp_lm(train_ifp="data/inputs/tapt/cc/fox", train_ofp="data/tapt/basil_train.txt"):
-    """
-    Preprocess commoncrawl data for tapt
-    """
-
-    files = os.listdir(train_ifp)
-    files = [fn for fn in files if not fn == 'stat.json']
-
-    for fn in files:
-        ifp = os.path.join(train_ifp, fn)
-        content = json.load(open(ifp))
-        text = content['maintext']
-        sentences = [s.text for s in nlp(text).sents]
-
-        with open(train_ofp, 'a') as f:
-            for s in sentences:
-                f.write(s)
-            f.write('\n')
-
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-add_use', '--add_use', action='store_true', default=False,
@@ -283,37 +169,15 @@ if __name__ == '__main__':
     if not os.path.exists('data/inputs/cim'):
         os.makedirs('data/inputs/cim')
 
-    TAPT_DATA_DIR = "experiments/tapt/basil_and_source_tapt/data/"
-    if not os.path.exists(TAPT_DATA_DIR):
-        os.mkdir(TAPT_DATA_DIR)
-
     basil = LoadBasil().load_basil_raw()
     basil.to_csv('data/basil.csv')
     basil = pd.read_csv('data/basil.csv', index_col=0).fillna('')
 
     # tokenize
-    #nlp = spacy.load("en_core_web_sm")
-    #basil['tokens'] = basil.sentence.apply(tokenize)
-    #basil.to_csv('data/inputs/basil_w_tokens.csv')
+    nlp = spacy.load("en_core_web_sm")
+    basil['tokens'] = basil.sentence.apply(tokenize)
+    basil.to_csv('data/inputs/basil_w_tokens.csv')
 
-    # ARTICLE & CONTEXT
+    # ARTICLE & EVENT CONTEXT
     # Groups basil instances by story and source, and write .tsv lines
-    #preprocess_for_cim(basil, add_use=False, add_sbert=False, ofp="data/inputs/cim/cim_basil.tsv")
-
-    # DOMAIN CONTEXT
-    # Split for tapt-ing
-
-    for src in [None, 'fox', 'nyt', 'hpo']:
-        preprocess_basil_for_dsp_lm(basil, eval_size=20, data_dir=TAPT_DATA_DIR, source=src)
-        preprocess_cc_for_dsp_lm(train_ifp=os.path.join(TAPT_DATA_DIR, source),
-                                 train_ofp=os.path.join(TAPT_DATA_DIR, source + '_train.txt'))
-
-    preprocess_basil_for_dsp_train(basil, data_dir="experiments/tapt/basil_and_source_tapt/data/", recreate=True)
-    exit(0)
-    # Split for source-specific tapt
-    for source in ['fox', 'nyt', 'hpo']:
-        #preprocess_basil_for_tapt(basil[basil['source'] == source], test_size=int(250 / 3), train_ofp="", test_ofp="data/inputs/tapt/basil_fox_test.txt")
-        preprocess_basil_for_dsp_train(basil[basil['source'] == source],
-                                       data_dir=f"experiments/dont-stop-pretraining/data/basil_{source}",
-                                       recreate=True, source=source)
-
+    preprocess_for_cim(basil, add_use=False, add_sbert=False, ofp="data/inputs/cim/cim_basil.tsv")
