991a2923776205c34f4b85c460a5e84eeab47061
vdberg <vdberg@cl.uni-heidelberg.de>
2021-01-29 11:42:36 +0100

    preprocess for domain adaptation

--- preprocessing/preprocess_for_tapt.py
+++ preprocessing/preprocess_for_tapt.py
@@ -39,7 +39,7 @@ def preprocess_basil_for_lm(basil_df, eval_size=20, data_dir="data/tapt/basil_an
 
     print("Basil for TAPT (run_language_modeling):")
     print(f'Wrote {len(train_df)} sentences to {train_ofp}')
-    print(f'Wrote {len(eval_df)} sentences to {eval_ofp}')
+    print(f'Wrote {len(eval_df)} sentences to {eval_ofp}\n')
 
 
 def load_cc_files(cc_dir):
@@ -75,7 +75,7 @@ def preprocess_cc_for_lm(cc_dir, tapt_dir, sources=('fox', 'nyt', 'hpo')):
                     f.write(s)
 
     print("CC for Cur TAPT (run_language_modeling):")
-    print(f'Wrote {len(cc_fns)} files to {ofp}')
+    print(f'Wrote {len(cc_fns)} files to {ofp}\n')
 
 
 def write_for_dsp_train(data, fp):
@@ -136,7 +136,7 @@ def preprocess_basil_for_dsp_train(data, data_dir, recreate=False, source=None):
 
     print("Basil for fine-tuning/eval (train):")
     print(f'Wrote {len(folds)} folds to {data_dir}')
-    print(f'Wrote {len(data_strs.keys())} folds stats to {data_str_fp}')
+    print(f'Wrote {len(data_strs.keys())} folds stats to {data_str_fp}\n')
 
     return folds
 
