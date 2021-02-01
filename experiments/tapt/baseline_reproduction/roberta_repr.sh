2d1bf43817d25dbb13331288abfbf81922f540c2
vdberg <vdberg@cl.uni-heidelberg.de>
2021-02-01 13:34:59 +0100

    baseline reproduction

---
+++ experiments/tapt/baseline_reproduction/roberta_repr.sh
@@ -0,0 +1,24 @@
+cd ../dont-stop-pretraining
+# RoBERTa on Sentence Split
+/opt/slurm/bin/srun --partition kama --gres=gpu:1  --mem 20GB python -m scripts.train --device 0 --perf +f1 --evaluate_on_test \
+                --hyperparameters ROBERTA_CLASSIFIER_MINI \
+                --config training_config/classifier.jsonnet \
+                --serialization_dir ../baseline_reproduction/roberta_sentence_split_ft_results/roberta_sentence_split \
+                --dataset basil_sentence_split \
+                --model roberta-base \
+                -x 11 22 33 44 55
+
+# RoBERTa on Story Split
+ITER=1
+MAXITER=11
+while [ "$ITER" -lt "$MAXITER" ];
+do
+/opt/slurm/bin/srun --partition kama --gres=gpu:1  --mem 20GB python -m scripts.train --device 0 --perf +f1 --evaluate_on_test \
+                --hyperparameters ROBERTA_CLASSIFIER_MINI \
+                --config training_config/classifier.jsonnet \
+                --serialization_dir '../baseline_reproduction/roberta_story_split_ft_results/roberta_'$ITER \
+                --dataset basil_$ITER \
+                --model roberta-base \
+                -x 11 22 33 44 55
+ITER=$((ITER+1));
+done
