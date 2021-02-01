8b9db88866c4bbe39b4f0bf681c978a1991d606c
vdberg <vdberg@cl.uni-heidelberg.de>
2021-02-01 13:16:34 +0100

    baseline reproduction

--- experiments/tapt/baseline_reproduction/baseline_reproduction.sh
+++ experiments/tapt/baseline_reproduction/baseline_reproduction.sh
@@ -1,13 +1,13 @@
-# BERT on BASIL
-/opt/slurm/bin/srun --partition kama --gres=gpu:1  --mem 20GB python -m scripts.train --device 0 --perf +f1 --evaluate_on_test \
-                --hyperparameters BERT_CLASSIFIER_MINI \
-                --config training_config/classifier.jsonnet \
-                --serialization_dir ../baseline_reproduction/bert_fan_ft_results/bert_sentence_split \
-                --dataset basil_sentence_split \
-                --model bert-base-cased \
-                -x 11 22 33 44 55
+# BERT on Sentence split
+#/opt/slurm/bin/srun --partition kama --gres=gpu:1  --mem 20GB python -m scripts.train --device 0 --perf +f1 --evaluate_on_test \
+#                --hyperparameters BERT_CLASSIFIER_MINI \
+#                --config training_config/classifier.jsonnet \
+#                --serialization_dir ../baseline_reproduction/bert_fan_ft_results/bert_sentence_split \
+#                --dataset basil_sentence_split \
+#                --model bert-base-cased \
+#                -x 11 22 33 44 55
 
-# BERT on BASIL
+# BERT on Story Split
 ITER=1
 MAXITER=11
 while [ "$ITER" -lt "$MAXITER" ];
