3ac340dcb79a07119ad4acfb4c07922bfbc97bf0
vdberg <vdberg@cl.uni-heidelberg.de>
2021-02-25 18:00:12 +0100

    eval update

--- experiments/tapt/eval/agg_eval.py
+++ experiments/tapt/eval/agg_eval.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     locations = args.results_dirs
     for loc in locations:
         result_dirn = os.path.basename(loc)
-        print(result_dirn)
+        print('loc:', loc)
         info = result_dirn[:-len('_ft_results')]
         info = info.split('_')
         model, split_type = '_'.join(info[:-2]), '_'.join(info[-2:])
