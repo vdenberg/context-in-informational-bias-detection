738667b6075a1576a49bd272b329e9cf0882eda6
vdberg <vdberg@cl.uni-heidelberg.de>
2021-02-25 18:04:38 +0100

    eval update

--- experiments/tapt/eval/agg_eval.py
+++ experiments/tapt/eval/agg_eval.py
@@ -1,5 +1,6 @@
 import argparse, os, json
 import pandas as pd
+pd.options.mode.chained_assignment = None  # default='warn'
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -39,8 +40,8 @@ if __name__ == "__main__":
         # format interesting col
         test_col = [i for i in all_df.columns if 'test' in i]
         int_col = ['seed', 'test_f1']
-        all_df.loc[:,test_col] = all_df[test_col].round(4) * 100
-        best_val_df.loc[:,test_col] = best_val_df[test_col].round(4) * 100
+        all_df[test_col] = all_df[test_col].round(4) * 100
+        best_val_df[test_col] = best_val_df[test_col].round(4) * 100
 
         # m and std
         all_descr = all_df[int_col].groupby('seed').mean().describe()
